99b88859034027541613119436e94ab9b8527121
Martin Holzhauer <martin@holzhauer.eu>
2022-04-08 17:28:01 +0200

    initial client struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
+#![allow(dead_code)]
+#[derive(Debug)]
+struct MocoAppClient {
+    api_key: String,
+    domain: String,
+}
+
+impl MocoAppClient {
+    pub fn new(api_key: String, domain: String) -> MocoAppClient {
+        MocoAppClient { api_key, domain }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let client = MocoAppClient::new("key".to_string(), "domain".to_string());
+        assert_eq!(client.api_key, "key");
+        assert_eq!(client.domain, "domain");
     }
 }
